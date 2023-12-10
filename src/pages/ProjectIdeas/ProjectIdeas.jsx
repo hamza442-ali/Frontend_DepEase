d7728f1d6299a7f6c9878f82bd761e765617d977
Mahad Rahat <95528513+Mahad-Rahat@users.noreply.github.com>
2023-12-10 14:48:30 +0500

    proIdeas and reqApprove

--- src/pages/ProjectIdeas/ProjectIdeas.jsx
+++ src/pages/ProjectIdeas/ProjectIdeas.jsx
@@ -1,31 +1,52 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ProjectIdeasList from '../../components/projectideas/ProjectIdeasList';
+import ProjectIdeaModal from '../../components/projectideas/ProjectIdeaModal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
-const projectIdeas = [
-  {
-    title: 'Project Idea 1',
-    description: 'devops project this is ',
-    teacherName: 'umer baiq',
-  },
-  {
-    title: 'Project Idea 2',
-    description: 'Ai base mobile app will recommend you palces to visist in city',
-    teacherName: 'Saad khan',
-  },
-  {
-    title: 'Project Idea 3',
-    description: 'Ai based app that will identity fake news',
-    teacherName: 'Naeem Akhtar',
-  },
 
-  
-  // Add more project ideas as needed
-];
 
 const ProjectIdeasPage = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [projectIdeas, setProjectIdeas] = useState([
+    {
+      title: 'Project Idea 1',
+      description: 'devops project this is ',
+      teacherName: 'umer baiq',
+    },
+    {
+      title: 'Project Idea 2',
+      description: 'Ai base mobile app will recommend you palces to visist in city',
+      teacherName: 'Saad khan',
+    },
+    {
+      title: 'Project Idea 3',
+      description: 'Ai based app that will identity fake news',
+      teacherName: 'Naeem Akhtar',
+    },
+  ]);
+
+  const addProjectIdea = (newProjectIdea) => {
+    setProjectIdeas([...projectIdeas, newProjectIdea]);
+  };
+
   return (
     <div className="container p-4 mx-auto">
-      <h1 className="mb-4 text-3xl font-semibold">Project Ideas</h1>
+      {/* ... (existing code) */}
+      <button
+        className="fixed flex items-center px-4 py-2 text-white bg-blue-500 rounded bottom-4 right-4"
+        onClick={() => setModalOpen(true)}
+      >
+        <FontAwesomeIcon icon={faPlus} className="mr-2" />
+        Add Project Idea
+      </button>
+      {isModalOpen && (
+        <ProjectIdeaModal
+          closeModal={() => setModalOpen(false)}
+          addProjectIdea={addProjectIdea}
+        />
+      )}
+      {/* Render the updated ProjectIdeasList component with the modified projectIdeas array */}
       <ProjectIdeasList projectIdeas={projectIdeas} />
     </div>
   );
