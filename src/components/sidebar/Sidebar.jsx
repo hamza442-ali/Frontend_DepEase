b2752fe0d4eabec8d4a3cee94d51b865316f2424
Mahad Rahat <95528513+Mahad-Rahat@users.noreply.github.com>
2023-12-11 21:41:30 +0500

    input and sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import person from "../../assets/images/person2.jpeg"
-import logo from "../../assets/images/logo.jpeg"
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../assets/images/logo.jpeg';
+
 library.add(fas);
 
-const Sidebar = ({ data}) => {
+const Sidebar = ({ data }) => {
   const [isExtended, setIsExtended] = useState(false);
-  const [activeMenu, setActiveMenu] = useState(null);
 
   const toggleSidebar = () => {
     setIsExtended(!isExtended);
@@ -22,12 +21,6 @@ const Sidebar = ({ data}) => {
   const handleMouseLeave = () => {
     setIsExtended(false);
   };
-
-  const handleMenuClick = (index) => {
-    setActiveMenu(activeMenu === index ? null : index);
-  };
-
-  
 
   return (
     <nav
@@ -44,67 +37,37 @@ const Sidebar = ({ data}) => {
           <div className="flex items-center">
             <div className="mr-2">
               <FontAwesomeIcon icon={logo} size="lg" />
-              {/* <img src={logo} alt="DepEase" className="w-10 h-8 mr-2 rounded-full" /> */}
             </div>
             <div className="font-semibold">Dep Ease</div>
           </div>
         ) : (
           <div className="mr-2">
             <FontAwesomeIcon icon="random" size="lg" />
-            {/* <img src={logo} alt="DepEase" className="w-10 h-8 mr-2 rounded-full" /> */}
           </div>
         )}
       </div>
       <ul>
         {data.map((item, index) => (
           <li key={index}>
-            <div
-              className={`${
-                isExtended
-                  ? 'flex items-center justify-between pl-6 pr-4 py-3 hover:bg-blue-600 hover:text-white cursor-pointer'
-                  : 'flex items-center p-4 hover:bg-blue-600 hover:text-white cursor-pointer'
-              }`}
-              onClick={() => handleMenuClick(index)}
-            >
-              <div className="flex items-center">
-                <span className="mr-2">
-                  <FontAwesomeIcon icon={item.icon} size="lg" />
-                </span>
-                <span>{isExtended && item.label}</span>
+            <Link to={item.path}>
+              <div
+                className={`${
+                  isExtended
+                    ? 'flex items-center justify-between pl-6 pr-4 py-3 hover:bg-blue-600 hover:text-white cursor-pointer'
+                    : 'flex items-center p-4 hover:bg-blue-600 hover:text-white cursor-pointer'
+                }`}
+              >
+                <div className="flex items-center">
+                  <span className="mr-2">
+                    <FontAwesomeIcon icon={item.icon} size="lg" />
+                  </span>
+                  <span>{isExtended && item.label}</span>
+                </div>
               </div>
-              {isExtended && item.submenus.length > 0 && (
-                <span>
-                  <FontAwesomeIcon
-                    icon={activeMenu === index ? 'angle-up' : 'angle-down'}
-                    size="xs"
-                  />
-                </span>
-              )}
-            </div>
-            {isExtended &&
-              item.submenus.length > 0 &&
-              activeMenu === index && (
-                <ul>
-                  {item.submenus.map((submenu, subIndex) => (
-                    <li key={subIndex}>
-                      <Link
-                        to={submenu.path}
-                        className="flex items-center py-3 pl-12 pr-4 hover:bg-blue-600 hover:text-white"
-                      >
-                        <span className="mr-2">
-                          <FontAwesomeIcon icon={submenu.icon} size="lg" />
-                        </span>
-                        <span>{submenu.label}</span>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              )}
+            </Link>
           </li>
         ))}
       </ul>
-
-      
     </nav>
   );
 };
